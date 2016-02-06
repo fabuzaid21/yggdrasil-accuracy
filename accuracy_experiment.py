f037f421cae865308b70335219902e324f5f6594
root <root@ip-172-31-55-25.ec2.internal>
2016-02-06 04:09:42 +0000

    generating data using tree

--- accuracy_experiment.py
+++ accuracy_experiment.py
@@ -3,9 +3,25 @@
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 import numpy as np
-from math import log
 
 import sys
+
+class Node:
+    def __init__(self, p, feature_index):
+        self.p = p
+        self.feature_index = feature_index
+
+    def predict(self, training_point):
+        if training_point[self.feature_index] > self.p:
+            if self.right is None:
+                return 1.0
+            else:
+                return self.right.predict(training_point)
+        else:
+            if self.left is None:
+                return 0.0
+            else:
+                return self.left.predict(training_point)
 
 if len(sys.argv) != 5:
     print 'Usage: python accuracy_experiment.py <# features> <# bins> <#training examples> <# trials>'
@@ -16,8 +32,17 @@ NUM_BINS = int(sys.argv[2])
 NUM_TRAINING_EXAMPLES = int(sys.argv[3])
 NUM_TRIALS = int(sys.argv[4])
 
+def generate_tree(depth):
+    if depth == 0:
+        return None
+    node = Node(np.random.uniform(), np.random.randint(NUM_FEATURES))
+    node.left = generate_tree(depth - 1)
+    node.right = generate_tree(depth - 1)
+    return node
+
 def single_run(num_bins):
-    t_truth = np.random.rand(1, NUM_FEATURES)
+    root = generate_tree(NUM_FEATURES)
+    #t_truth = np.random.rand(1, NUM_FEATURES)
     #print 't_truth: ', t_truth
     features = np.random.rand(NUM_TRAINING_EXAMPLES, NUM_FEATURES)
     #print features
@@ -28,13 +53,15 @@ def single_run(num_bins):
     X_train_discretized = [[bins[i] for i in np.digitize(row, bins)] for row in features]
     #print X_train_discretized
 
-    if NUM_FEATURES == 1:
-        y_train = [1.0 if row[0] else 0.0
-                   for row in features > t_truth]
-    else:
-        y_train = [1.0 if reduce(lambda a, b: np.logical_xor(a, b), row, False)
-                       else 0.0
-                       for row in features > t_truth]
+    y_train = [root.predict(row) for row in features]
+
+    #if NUM_FEATURES == 1:
+    #    y_train = [1.0 if row[0] else 0.0
+    #               for row in features > t_truth]
+    #else:
+    #    y_train = [1.0 if reduce(lambda a, b: np.logical_xor(a, b), row, False)
+    #                   else 0.0
+    #                   for row in features > t_truth]
     #print y_train
 
     regr = DecisionTreeClassifier(max_depth=NUM_FEATURES)
